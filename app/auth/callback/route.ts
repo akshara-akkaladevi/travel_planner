c0af0b6ca8f2baf4b85b87c9a7450488922fe483
Atharva <dagaonkaratharva@gmail.com>
2024-05-28 21:27:52 +0530

    Fixed the route for auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,13 +1,13 @@
-import {createRouteHandlerClient} from '@supabase/auth-helpers-nextjs'
-import {cookies} from 'next/headers'
-import {NextResponse} from "next/server";
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse, NextRequest } from 'next/server'
 
-export async function GET(request: { url: string | URL; }) {
+export async function GET(request: NextRequest) {
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get('code');
-    if(code) {
+    if (code) {
         const cookieStore = cookies();
-        const supabase = createRouteHandlerClient({ cookies: () => cookieStore})
+        const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
         await supabase.auth.exchangeCodeForSession(code);
     }
 
